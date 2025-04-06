78d0fab55ccda5dd9dbf381706bd223327f056ab
Trung Pham <tqpham@nvidia.com>
2025-04-06 16:43:21 +0700

    fix: add input output central registry

--- src/donate_a_pytest/model.py
+++ src/donate_a_pytest/model.py
@@ -1,10 +1,60 @@
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import Optional, List, Dict, Any
+import logging
 
 class TestCase(BaseModel):
-    input: dict
-    output: dict
-    description: Optional[str] = None
+    inp: dict = Field(alias="input")
+    outp: dict = Field(alias="output")
+    desc: Optional[str] = Field(alias="description")
 
+class InputOutputRegistry:
+    """
+    Singleton class to manage registered test functions and their test cases.
+    """
+    _instance = None
 
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(InputOutputRegistry, cls).__new__(cls)
+            cls._instance._test_cases = {}
+        return cls._instance
+    
+    def __init__(self) -> None:
+        raise Exception("The class is a singleton, use get_instance() instead")
 
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = cls.__new__(cls)
+        return cls._instance
+        
+    
+    def register(self, func_name: str="", func: callable=None, inp: dict=None, outp: dict=None, desc: Optional[str] = None) -> None:
+        """Register a test function"""
+        logger = logging.getLogger(__name__)
+
+        if func_name:
+            test_name = func_name
+        elif func:
+            test_name = func.__name__
+        else:
+            raise ValueError("Either func_name or func must be provided")
+
+        for test_case in self._test_cases.get(test_name, []):
+            if test_case.inp == inp and test_case.outp == outp and test_case.desc == desc:
+                logger.info(f"Test case already registered: {test_name}")
+                return
+            
+        tests_cases = self._test_cases.get(test_name, [])
+        tests_cases.append(TestCase(inp=inp, outp=outp, desc=desc))
+
+        self._test_cases[test_name] = tests_cases
+    
+    def get(self, func_name: str = "", func: callable = None) -> callable:
+        """Get a test function by name"""
+        if func_name:
+            return self._test_cases.get(func_name, [])  
+        elif func:
+            return self._test_cases.get(func.__name__, [])
+        else:
+            raise ValueError("Either func_name or func must be provided")
--- src/donate_a_pytest/tests_crawler.py
+++ src/donate_a_pytest/tests_crawler.py
@@ -1,14 +1,16 @@
 import os
 import json
 import yaml
-
+from itertools import chain
 from donate_a_pytest.utils import find_paths_with_substring
+from donate_a_pytest.model import TestCase, InputOutputRegistry
 
-def crawl_json_test_cases(test_name: str) -> list:
+def crawl_json_test_cases(test_name: str, search_dir: str = None) -> list:
     """
     Crawl the json input for a given test name
     """
-    json_files = find_paths_with_substring(os.path.dirname(__file__),test_name, ".json")
+    search_dir = search_dir or os.getcwd()
+    json_files = find_paths_with_substring(search_dir,test_name, ".json")
     test_cases = []
     for json_file in json_files:
 
@@ -18,11 +20,12 @@ def crawl_json_test_cases(test_name: str) -> list:
     
     return test_cases
 
-def crawl_yaml_test_cases(test_name: str) -> list:
+def crawl_yaml_test_cases(test_name: str, search_dir: str = None) -> list:
     """
     Crawl the yaml input for a given test name
     """
-    yaml_files = find_paths_with_substring(os.path.dirname(__file__),test_name, ".yaml")
+    search_dir = search_dir or os.getcwd()
+    yaml_files = find_paths_with_substring(search_dir, test_name, ".yaml")
     test_cases = []
     for yaml_file in yaml_files:
         with open(yaml_file, "r", encoding="utf-8") as f:
@@ -30,3 +33,25 @@ def crawl_yaml_test_cases(test_name: str) -> list:
             test_cases.append(data)
     
     return test_cases
+
+def get_test_cases(func_name: str= "", func: callable = None) -> list:
+    """
+    Get the test cases for a given function name or function
+    """
+    name = ""
+    if func_name:
+        name = func_name
+    elif func:
+        name = func.__name__
+    else:
+        raise ValueError("Either func_name or func must be provided")
+    
+    # Crawl the test cases from the json and yaml files
+    json_test_cases = crawl_json_test_cases(name)
+    yaml_test_cases = crawl_yaml_test_cases(name)
+    for test_case in chain(json_test_cases, yaml_test_cases):
+        test_case = TestCase(**test_case)
+        InputOutputRegistry.get_instance().register(name, test_case.inp, test_case.outp, test_case.desc)
+    
+    return InputOutputRegistry.get_instance().get(name)
+    
